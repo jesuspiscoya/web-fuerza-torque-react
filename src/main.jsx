7c5fa80d97e43eea408463e93e3776188a6aac61
Jesus Piscoya <jesus.piscoya@laive.pe>
2024-10-28 13:06:05 -0500

    Add view on Home Page

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/web-fuerza-torque">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,75 @@ import { Slider } from "../components/Slider";
 import { Carousel } from "../components/Carousel";
 import { ScrollAnimation } from "../components/ScrollAnimation";
 import { Footer } from "../components/Footer";
+import img1 from "../assets/mantenimiento2.jpg";
+import img2 from "../assets/industrias2.jpg";
+import img3 from "../assets/herramientas.webp";
 
 export const Home = () => {
-    return (
+  const services = [
+    {
+      title: "Mantenimiento",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, suscipit.",
+      image: img1,
+    },
+    {
+      title: "Reparación",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, suscipit.",
+      image: img2,
+    },
+    {
+      title: "Alquiler",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, suscipit.",
+      image: img3,
+    },
+  ];
+
+  return (
     <>
       <Slider />
       <Navbar />
+      <div className="bg-gradient-to-t to-gray-900 from-blue-gray-900 text-center p-7 md:p-16">
+        <ScrollAnimation to="top">
+          <span className="text-yellow-700 rubik font-semibold">
+            Más de 5 años de experiencia
+          </span>
+        </ScrollAnimation>
+        <ScrollAnimation to="left" delay={300}>
+          <h1 className="text-4xl text-white rubik font-bold mt-3 mb-16">
+            Nuestros Servicios
+          </h1>
+        </ScrollAnimation>
+        <div className="container flex">
+          {services.map((service, index) => (
+            <ScrollAnimation
+              to="bottom"
+              key={index}
+              className="flex-1 flex flex-col gap-5 md:gap-10 text-center items-center"
+              delay={index * 100 + 500}
+            >
+              <div className="relative overflow-hidden hover:scale-[1.02] hover:-translate-y-2 transition-all duration-500">
+                <img
+                  src={service.image}
+                  alt={service.title}
+                  className="h-80 w-80 object-cover"
+                />
+                <div className="absolute opacity-0 pt-52 hover:pt-10 hover:opacity-100 flex flex-col gap-5 bg-black/80 top-0 bottom-0 justify-center items-center text-center transition-all duration-500 p-10 text-sm">
+                  <h1 className="text-red-700 font-semibold text-xl">
+                    {service.title}
+                  </h1>
+                  <p className="text-gray-400">{service.description}</p>
+                  <button className="text-white hover:text-yellow-700 font-semibold transition-all duration-500 px-3 py-1 rounded-lg">
+                    Ver más
+                  </button>
+                </div>
+              </div>
+            </ScrollAnimation>
+          ))}
+        </div>
+      </div>
       <div className="relative flex min-h-screen md:overflow-hidden">
         <div className="parallax-bg" />
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/75">
