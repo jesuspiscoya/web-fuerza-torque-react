89ce12c8176fc1acfe8382a9a656e5753010b3a8
Jesus Piscoya <jesus.piscoya@laive.pe>
2024-10-27 23:23:13 -0500

    Add basename parameter in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web-fuerza-torque">
       <App />
     </BrowserRouter>
   </StrictMode>
